6a14601c2709fbf87f575c137bca7abd76048953
DanielG <dgrecug@gmailc.com>
2022-08-03 12:25:09 +0300

    added no arguments to print but not working

--- 05_show_args/src/main_show_args.cpp
+++ 05_show_args/src/main_show_args.cpp
@@ -3,7 +3,10 @@
 int main(int argc, char** args)
 {
 	if (argc > 1)
+	{
 		for (int i = 1; i < argc; i++)
 			std::cout << args[i] << "\n";
+	}
+	else std::cout << "No arguments to print\n";
 	return 0;
 }
